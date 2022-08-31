7d79bd76e14a46fbb85d9be2f9caade84a0a6949
ghkim <gkim9213@gmail.com>
2022-08-31 11:47:01 +0900

    Fix for websocket

--- dashboard/routing.py
+++ dashboard/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .consumers import StkrptArConsumer
 
 websocket_urlpatterns = [
-    path('ws/dashboard/stkrpt/ar/<stock_code>/', StkrptArConsumer.as_asgi()),
+    path('ws/dashboard/stkrpt/ar/<stock_code>', StkrptArConsumer.as_asgi()),
 ]
